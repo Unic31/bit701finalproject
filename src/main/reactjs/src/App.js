82bb2203f72b044e8c5994915f96b89d70bba67f
Unic31 <uenic31@gmail.com>
2023-06-21 16:37:40 +0900

    123

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import img from './goodman.jpg'
+
+function App() {
+  return (
+    <div className="App">
+       <br/><br/>
+      <img alt='' className='App-logo' src={img} width={300} border="2" />
+      <br/><br/>
+      <h1>쌍따봉 적립</h1>
+    </div>
+  );
+}
+
+export default App;
